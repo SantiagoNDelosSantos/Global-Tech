510bdbd1667fae05be563b79591c144e13a11b4d
Santiago Nahuel De los Santos <santiagodelossantos630@gmail.com>
2023-03-15 18:56:38 -0300

    PreEntrega1 + De los Santos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Counter from './components/Counter/Counter';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
+
         <Navbar/>
+
+        {/*<Counter/>*/}
+
+        <ItemListContainer greeting={"Bienvenidos a GlobalTech."}/>
+    
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,32 @@
 import "./Navbar.css"
+import Button from "../Button/Button"
+import CarWidget from "../CartWidget/CartWidget"
 
 const Navbar = ()  => {
     return(
+
         <nav className="Navbar">
-            <h1>
+
+            <h1 className="Logo"> 
                 Global Tech
             </h1>
-            <div>
-                <button>Celulares</button>
-                <button>Tables</button>
-                <button>Laptops</button>
-                <button>Monitores</button>
-                <button>Periféricos</button>
+
+            <div className="AllBtn">
+
+                <Button label="Celulares" callback={() =>console.log("Cel")}/>
+                <Button label="Tables" callback={() =>console.log("Tab")}/>
+                <Button label="Laptops" callback={() =>console.log("Lap")}/>
+                <Button label="Monitores" callback={() =>console.log("Mon")}/>
+                <Button label="Periféricos" callback={() =>console.log("Perif")}/>
+
             </div>
+
+            <div>
+                <CarWidget/>
+            </div>
+
         </nav>
+
     )
 }
 
